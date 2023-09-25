d09dc1b83a048431b70be433c05ed59e091eff8c
João Victor Piekarski Chaves <yungchaves@gmail.com>
2023-09-25 15:12:01 -0300

    meu primeiro commit

---
+++ Modulo01/Aula01/Game.java
@@ -0,0 +1,15 @@
+package Modulo01;
+
+public class Game {
+
+    public static void main(String[] args) {
+        String nome = "Joao";
+        int idade = 22;
+        double peso = 85.0;
+        boolean tipo = true;
+        System.out.println("Hello world " + nome );
+        System.out.println("A Idade e:" + idade );
+        System.out.println("O peso e: " + peso);
+    }
+
+}
---
+++ Modulo01/Aula02/Game.java
@@ -0,0 +1,17 @@
+package Modulo02;
+
+public class Game {
+
+    public static void main(String[] args) {
+        String nome = "Joao";
+        int idade = 22;
+        double peso = 85.0;
+        boolean tipo = true;
+        
+        if(nome.equals("Joao")) {
+            System.out.println("Verdade!");
+        }else {
+            System.out.println("A idade correta é "+ idade);
+        }
+        }
+    }
