eb0a52be8e7efb091aa80f578d00aa3e31740360
João Victor Piekarski Chaves <yungchaves@gmail.com>
2023-09-25 15:58:27 -0300

    Minha mensagem aqui

---
+++ Modulo01/Aula03/Game.java
@@ -0,0 +1,49 @@
+package Aula03;
+
+public class Game {
+    
+    public static void main(String[] args) {
+
+        int contador = 1;
+        
+        // while(contador <= 10) {
+            //inicio = valor
+            // No while definir o inicio condição
+            //while(condição)
+            // o que fazer 
+            //passo_condição(++) => aumentar
+            //passo_condição(--) => diminuir
+
+            //while ( inicio > limite) {
+  // o que fazer
+//   inicio-- //diminuir
+// }
+
+
+//while ( inicio < limite) {
+  // o que fazer
+  //inicio++ //aumentar
+// }
+
+//for (incio; incio > limite; incio--) {
+  // o que fazer
+  // diminuindo
+// }
+
+//for (incio; incio < limite; incio++) {
+  // o que fazer
+  // aumentando
+
+        //     System.out.println("O numero atual e: "+ contador);
+        
+        
+        //     contador++; 
+        // for (Inicio; limite/final; passo)
+        // }
+        for(int i=2; i <= 10; i+=2){
+         System.out.println("O numero atual e: "+ i);
+        
+       }
+    }
+    
+}
